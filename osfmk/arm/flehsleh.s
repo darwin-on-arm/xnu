4eb8cda8c17845aebcf8e1a6bbc9120bb82404d2
rms <winocm@icloud.com>
2013-08-19 00:38:48 -0500

    I changed my email address.

--- osfmk/arm/flehsleh.s
+++ osfmk/arm/flehsleh.s
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013, winocm. <rms@velocitylimitless.org>
+ * Copyright 2013, winocm. <winocm@icloud.com>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification,
