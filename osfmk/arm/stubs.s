24f8136b3b76664da0cbee98a1c1cf11ffe6aa8d
b-man <mckenzba@gmail.com>
2013-09-21 23:12:36 +0000

    Apply printf bug workaround, implement setjmp and longjmp for arm

--- osfmk/arm/stubs.s
+++ osfmk/arm/stubs.s
@@ -59,9 +59,7 @@
 UNIMPLEMENTED_STUB(_LockTimeOut)
 
 UNIMPLEMENTED_STUB(__MachineStateCount)
-UNIMPLEMENTED_STUB(__longjmp)
 UNIMPLEMENTED_STUB(__serial_getc)
-UNIMPLEMENTED_STUB(__setjmp)
 
 UNIMPLEMENTED_STUB(_aout_db_init)
 UNIMPLEMENTED_STUB(_aout_db_line_at_pc)
